2cbd5a8dae5a7a224c88cdd987bd8c3e8932900d
Robson Correia <robson.correia.bd@gmail.com>
2022-10-18 10:48:53 -0300

    feta: Matching BasicPatterns

--- Start/PatternMatching/BasicPatterns/Program.cs
+++ Start/PatternMatching/BasicPatterns/Program.cs
@@ -8,37 +8,47 @@ string? str = "This is a string";
 
 // The "is" expression can also be used to extract a value if one is present
 // This is called the Declaration pattern
-void dashedline(object o) {
+void dashedline(object o)
+{
     // The old way of doing this is to try determine the type of the argument
-    int l = 0;
-    if (o.GetType() == typeof(int)) {
-        l = (int)o;
-    }
-    string s;
-    if (o.GetType() == typeof(string)) {
-        s = (string)o;
-        if (!int.TryParse(s, out l)) {
-            l = 0;
-        }
-    }
-    if (l > 0) {
-        string str = new string('-', l);
-        Console.WriteLine(str);
-    }
-
+    // int l = 0;
+    // if (o.GetType() == typeof(int))
+    // {
+    //     l = (int)o;
+    // }
+    // string s;
+    // if (o.GetType() == typeof(string))
+    // {
+    //     s = (string)o;
+    //     if (!int.TryParse(s, out l))
+    //     {
+    //         l = 0;
+    //     }
+    // }
+    // if (l > 0)
+    // {
+    //     string str = new string('-', l);
+    //     Console.WriteLine(str);
+    // }
     // TODO: The new way is to just use the declaration pattern
+    if (o is int l || (o is string s && int.TryParse(s, out l)))
+    {
+        string str = new string('_', l);
+        System.Console.WriteLine(str);
+    }
 }
 
 // dashedline(25);
 // dashedline("50");
 // dashedline(20.5);
 
-// Property pattern examines the properties of an object
-// bool IsTheIdesOfMarch(DateTime date) {
-//     // Test the month and day properties
-//     return false;
-// }
-// Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 1, 13)));
-// Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 14)));
-// Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 15)));
-// Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 16)));
+//Property pattern examines the properties of an object
+bool IsTheIdesOfMarch(DateTime date) {
+    // Test the month and day properties
+    return (date is {Month: 3, Day: 15 or 14});
+}
+
+Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 1, 13)));
+Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 14)));
+Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 15)));
+Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 16)));

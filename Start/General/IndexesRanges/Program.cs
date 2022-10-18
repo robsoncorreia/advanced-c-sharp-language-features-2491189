8c91fa3c232ec02ded4c2e107bceac604c7197c9
Robson Correia <robson.correia.bd@gmail.com>
2022-10-18 00:21:41 -0300

    feat: number literals

--- Start/General/IndexesRanges/Program.cs
+++ Start/General/IndexesRanges/Program.cs
@@ -5,7 +5,7 @@
 string[] words = new string[]
 {
     "The","quick","brown","fox","jumps",
-    "over","the","lazy","dog"       
+    "over","the","lazy","dog"
 };
 
 // The index operator provides access to array elements
@@ -14,7 +14,22 @@ Console.WriteLine(words[1]);
 // TODO: The index-from-end operator indexes from the end of a sequence
 // (Note that the end is not zero-based, ^1 is the last item)
 
+Console.WriteLine(words[^1]);
+
 // TODO: The range operator (..) defines a range
 
+string[] wordRange = words[2..5];
+System.Console.WriteLine($"{string.Join(",", wordRange)}");
+wordRange = words[..];
+System.Console.WriteLine($"{string.Join(",", wordRange)}");
+wordRange = words[2..];
+System.Console.WriteLine($"{string.Join(",", wordRange)}");
+wordRange = words[..5];
+System.Console.WriteLine($"{string.Join(",", wordRange)}");
 
 // TODO: Indexes and ranges can be variables too
+Index idx = ^4;
+System.Console.WriteLine( words[idx]);
+Range rng = 3..^1;
+wordRange = words[rng];
+System.Console.WriteLine($"{string.Join(",", wordRange)}");
--- Start/General/NumberLiterals/Program.cs
+++ Start/General/NumberLiterals/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Example file for LinkedIn Learning Course Advanced C#: Language Features by Joe Marini
 
 // TODO: Starting in C# 7, you can use _ as a number separator
-int d = 123456;
-float f = 1234.5f;
-var x = 0xABCDEF;
-var b = 0b1101111010010010;
+int d = 123_456;
+float f = 1_234.5f;
+var x = 0xAB_CD_EF;
+var b = 0b_1101_1110_1001_0010;
 
 Console.WriteLine($"{d}");
 Console.WriteLine($"{f}");

0ea3ff63f6d0e6dae46bb18689338d7a07b7b382
Robson Correia <robson.correia.bd@gmail.com>
2022-10-18 19:59:20 -0300

    feat: comparing

--- Start/Classes/Comparing/Program.cs
+++ Start/Classes/Comparing/Program.cs
@@ -2,8 +2,10 @@
 // Comparing objects for equality
 
 // Declare some Point2D instances
-Point2D p1 = new Point2D() {X = 10, Y = 20};
-Point2D p2 = new Point2D() {X = 10, Y = 20};
+using System.Collections;
+
+Point2D p1 = new Point2D() { X = 10, Y = 20 };
+Point2D p2 = new Point2D() { X = 10, Y = 20 };
 Point2D p3 = null, p4 = null;
 
 // Test for equality
@@ -13,13 +15,28 @@ Console.WriteLine($"{p1 != p2}");
 Console.WriteLine($"{p3 == p4}");
 
 // TODO: The IEquatable interface gives us a type-specific Equals() method
-public class Point2D {
+public class Point2D : IEquatable<Point2D>
+{
     public int X;
     public int Y;
 
+
     // TODO: Override the methods needed to define value equality
-
-
+    public override bool Equals(object? obj) => Equals(obj as Point2D);
+    public bool Equals(Point2D? p) => p != null && (X == p.X && Y == p.Y);
+    public override int GetHashCode() => (X, Y).GetHashCode();
     // TODO: Override the == and != operators as well
-
+    public static bool operator ==(Point2D? p1, Point2D? p2)
+    {
+        if (p1 is null)
+        {
+            return false;
+        }
+        if (p2 is null)
+        {
+            return false;
+        }
+        return p1.Equals(p2);
+    }
+    public static bool operator !=(Point2D? p1, Point2D? p2) => !(p1 == p2);
 }
